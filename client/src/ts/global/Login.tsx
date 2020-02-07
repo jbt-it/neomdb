eb815847762308138780bf2a009d727e68b848db
christophtrybek <christoph.trybek@mailbox.org>
2020-02-07 14:05:26 +0100

    Added login component

--- client/src/ts/global/Login.tsx
+++ client/src/ts/global/Login.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Textfield from "@material-ui/core/TextField";
 
 const Login: React.FunctionComponent = () => {
 
+  const [username, setUsername] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+
+  /**
+   * Handles the API call and cleans state thereafter
+   */
+  const login: VoidFunction = () => {
+    // TODO: Implement API call here
+    setUsername(null);
+    setPassword(null);
+  };
+
   return (
-    <div className="App">
-      <h1>Login</h1>
+    <div className="login">
+      <Card className="card">
+        <CardContent>
+          <h1>Login</h1>
+          <form className="login-form">
+            <Textfield id="username" label="Benutzername" type="text"
+              onChange = {event => {setUsername(event.target.value);}}/>
+            <Textfield id="password" label="Passwort" type="password"
+              onChange = {event => {setPassword(event.target.value);}}/>
+          </form>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" size="small" color="primary"
+            onClick = {event => {login();}}>
+            Login
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
