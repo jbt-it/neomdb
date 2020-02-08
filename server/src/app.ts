4e340d83d6163cd6eaf7222d5d044dbd824ed199
christophtrybek <christoph.trybek@mailbox.org>
2020-02-08 15:19:17 +0100

    Added processing for post requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,27 @@ import dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-
 app.set("port", process.env.PORT);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.urlencoded());
+app.use(express.json());
+
+/**
+ * Enable CORS for all incoming requests
+ */
+app.all("/*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+/**
+ * Handle login request and respond with entered data
+ */
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 export default app;
