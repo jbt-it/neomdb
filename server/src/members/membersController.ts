b0454a1653ebfb16fdbbbfc8e6a78178b02de14f
christophtrybek <christoph.trybek@mailbox.org>
2020-02-11 19:11:20 +0100

    Added database connection pooling with promises

--- server/src/members/membersController.ts
+++ server/src/members/membersController.ts
@@ -2,8 +2,9 @@
  * Definition of the handler functions for the users module
  */
 import {Request, Response} from "express";
+import database = require("../database");
 
-import * as usersTypes from "members/membersTypes";
+import * as membersTypes from "members/membersTypes";
 
 /**
  * Handles transmitted credentials and replies accordingly
@@ -16,10 +17,16 @@ export const replyCredentials = (req: Request, res: Response): void => {
  * Checks if complete credenials were specified
  * @param credentials Credentials specified by the user
  */
-const checkCredentials = (credentials: usersTypes.Credentials): string | usersTypes.Credentials => {
+const checkCredentials = (credentials: membersTypes.Credentials): string | membersTypes.Credentials => {
  if(credentials.username === "" || credentials.password === ""){
    return("credetials are not complete");
  } else {
-  return(credentials);
+   database.query("SELECT * FROM mitglied WHERE name = ? AND passwort = ?", [credentials.username, credentials.password])
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log("Error " + err);
+    });
  }
 };
