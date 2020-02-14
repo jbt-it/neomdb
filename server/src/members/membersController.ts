d6b0f5b0184753c300788198d2a287fe6beeab7b
christophtrybek <christoph.trybek@mailbox.org>
2020-02-14 23:00:02 +0100

    Verify password hash using bcrypt

--- server/src/members/membersController.ts
+++ server/src/members/membersController.ts
@@ -3,6 +3,7 @@
  */
 import database = require("../database");
 import auth = require("../global/authController");
+import bcrypt = require("bcryptjs");
 
 import {Request, Response} from "express";
 import * as membersTypes from "./membersTypes";
@@ -13,19 +14,21 @@ import {JWTPayload} from "../global/globalTypes";
  */
 export const replyCredentials = (req: Request, res: Response): void => {
  if(req.body.username === "" || req.body.password === ""){
-   res.status(400).send("Credentials incomplete");
+   res.status(401).send("Credentials incomplete");
  } else {
    database.query(
-    `SELECT mitgliedID, name, GROUP_CONCAT(mitglied_has_berechtigung.berechtigung_berechtigungID) AS permissions
+    `SELECT mitgliedID, name, passwordHash, GROUP_CONCAT(mitglied_has_berechtigung.berechtigung_berechtigungID) AS permissions
     FROM mitglied
     INNER JOIN mitglied_has_berechtigung ON mitglied.mitgliedID = mitglied_has_berechtigung.mitglied_mitgliedID
-    WHERE mitglied.name = ? AND mitglied.passwordHash = ?
+    WHERE mitglied.name = ?
     GROUP BY mitgliedID, name`,
-    [req.body.username, req.body.password])
+    [req.body.username])
     .then((result: membersTypes.LoginQueryResult[]) => {
       if (result.length === 0) {
         res.status(401).send("Username or password wrong");
       }
+      bcrypt.compare(req.body.password, result[0].passwordHash)
+      .then((match) => {
       const payload: JWTPayload = {
         mitgliedID: result[0].mitgliedID,
         name: result[0].name,
@@ -33,7 +36,11 @@ export const replyCredentials = (req: Request, res: Response): void => {
       };
       res.status(200).json(auth.generateJWT(payload));
     })
-    .catch(err => {
+      .catch((err) => {
+        res.status(401).send("Username or password wrong");
+      });
+    })
+    .catch((err) => {
       res.status(500).send("Query Error");
     });
  }
