69b60da31f9838f0fd3e0f53b94dc6058bf1b2ee
Lennart Rukower <l.rukower@studentische-beratung.de>
2020-03-10 22:40:45 +0100

    Extracted inline-css from Nav to external scss file

--- client/src/ts/members/Navigation/Nav.tsx
+++ client/src/ts/members/Navigation/Nav.tsx
@@ -4,81 +4,16 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
-import {
-  createStyles,
-  fade,
-  Theme,
-  makeStyles
-} from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import MenuDrawer from "./MenuDrawer";
-
-// TODO: convert into scss
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    flexGrow: 1
-  },
-  bar: {
-    backgroundColor: "rgb(246,137,31)"
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1,
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: 120,
-      "&:focus": {
-        width: 200
-      }
-    }
-  }
-}));
 
 /**
  * The navigation of the application
  */
 const Nav:React.FunctionComponent = () => {
-
-  const classes = useStyles();
-
   const [isMenuDrawerOpen, setIsMenuDrawerOpen] = React.useState(false);
+  const [isSearchBarActive, setIsSearchBarActive] = React.useState(false);
 
   /**
    * Handles the toogle drawer event
@@ -100,37 +35,60 @@ const Nav:React.FunctionComponent = () => {
     setIsMenuDrawerOpen(open);
   };
 
-  return (<div>
-    <AppBar position="static" className={classes.bar}>
-  <Toolbar>
-    <IconButton
-      edge="start"
-      className={classes.menuButton}
-      color="inherit"
-      aria-label="open drawer"
-      onClick={toggleDrawer(true)}
-    >
-      <MenuIcon />
-    </IconButton>
-    <Typography className={classes.title} variant="h6" noWrap>
-      JBT neoMDB
-    </Typography>
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
-      <InputBase
-        placeholder="Suche..."
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputInput
-        }}
-        inputProps={{ "aria-label": "search" }}
-      />
-    </div>
-  </Toolbar>
-</AppBar>
-<MenuDrawer open={isMenuDrawerOpen} drawer={toggleDrawer}/>
+  /**
+   * Returns the scss-class for the search bar
+   */
+  const getClassForSearchBar = () => {
+    if(isSearchBarActive) {
+      return "search-active";
+    } else {
+      return "search";
+    }
+  };
+
+  return (<div className="bar-root">
+    <AppBar position="static" className="bar">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className="menu-button"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography 
+          className="title" 
+          variant="h6" noWrap
+        >
+          JBT neoMDB
+        </Typography>
+        <div className={getClassForSearchBar()}>
+          <div className="search-icon">
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Suche..."
+            classes={{
+              root: "search-input-root",
+              input: "search-input-input"
+            }}
+            inputProps={{ "aria-label": "search" }}
+            onFocus={() => {
+              setIsSearchBarActive(true);
+            }}
+          onBlur={() => {
+            setIsSearchBarActive(false);
+          }}
+          />
+        </div>
+      </Toolbar>
+  </AppBar>
+<MenuDrawer
+  open={isMenuDrawerOpen}
+  drawer={toggleDrawer}
+/>
 </div>);
 };
 
