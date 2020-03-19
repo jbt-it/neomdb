bcbb686bf2f6ce5fc7803721a3e49d53e0c67398
Lennart Rukower <l.rukower@studentische-beratung.de>
2020-03-19 18:39:25 +0100

    Added nav and routes to router

--- client/src/ts/global/App.tsx
+++ client/src/ts/global/App.tsx
@@ -3,13 +3,29 @@ import {HashRouter, Route, Switch} from "react-router-dom";
 
 import Dashboard from "../members/Dashboard";
 import Login from "../members/Login";
+import Nav from "./navigation/Nav";
 
 const App: React.FunctionComponent = () => {
 
   return (
     <HashRouter>
+      <Nav/>
       <Switch>
         <Route exact path = "/" component = {Dashboard} />
+        <Route exact path = "/gesamtuebersicht" component = {Dashboard} />
+        <Route exact path = "/vorstand" component = {Dashboard} />
+        <Route exact path = "/geburtstage" component = {Dashboard} />
+        <Route exact path = "/traineebereich" component = {Dashboard} />
+        <Route exact path = "/kuratoren" component = {Dashboard} />
+        <Route exact path = "/projekte" component = {Dashboard} />
+        <Route exact path = "/veranstaltungen" component = {Dashboard} />
+        <Route exact path = "/mm-tracking" component = {Dashboard} />
+        <Route exact path = "/pl-qm-tool" component = {Dashboard} />
+        <Route exact path = "/raumreservierung" component = {Dashboard} />
+        <Route exact path = "/innovationsmanagement" component = {Dashboard} />
+        <Route exact path = "/meine-funktionen" component = {Dashboard} />
+        <Route exact path = "/weitere-funktionen" component = {Dashboard} />
+        <Route exact path = "/kvp" component = {Dashboard} />
         <Route exact path = "/login" component = {Login} />
       </Switch>
     </HashRouter>
