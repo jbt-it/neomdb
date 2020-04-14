318ca26e69c86d45d5c39bbedee5c25be33402fc
qnewald <newaldquentin@googlemail.com>
2020-04-14 20:46:40 +0200

    Reviewed code

--- client/src/ts/members/Login.tsx
+++ client/src/ts/members/Login.tsx
@@ -1,17 +1,16 @@
 /**
  * Component that handles the login process
  */
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import api from '../utils/api';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import api from "../utils/api";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Textfield from "@material-ui/core/TextField";
-import { makeStyles } from '@material-ui/core/styles';
-import logo from '../../images/jbt-logo-black.png';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import logo from "../../images/jbt-logo-black.png";
+import Link from "@material-ui/core/Link";
 
 const Login: React.FunctionComponent = () => {
 
@@ -22,21 +21,21 @@ const Login: React.FunctionComponent = () => {
     paper: {
       marginTop: theme.spacing(8),
       paddingBottom: theme.spacing(3),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
     },
     logo: {
-      width: '30%',
-      height: 'auto',
+      width: "30%",
+      height: "auto",
       marginTop: theme.spacing(3),
     },
     login: {
-      width: '65%',
+      width: "65%",
       marginTop: theme.spacing(1),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
     },
     inputfield: {
     },
@@ -44,25 +43,24 @@ const Login: React.FunctionComponent = () => {
       margin: theme.spacing(3, 0, 1),
     },
     linkItem: {
-      display: 'flex',
-      justifyContent: 'flex-start',
-      alignItems: 'center',
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "center",
     },
     warningItem: {
-      display: 'flex',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "center",
     },
     warningText: {
       color: theme.palette.error.main,
-      textDecoration: 'none',
-      pointerEvents: 'none',
+      textDecoration: "none",
+      pointerEvents: "none",
     },
   }));
 
   const classes = useStyles();
-  
-  
+
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [capslock, setCapslock] = useState<boolean>(false);
@@ -73,56 +71,56 @@ const Login: React.FunctionComponent = () => {
    */
   const login: VoidFunction = () => {
     api.post("/users/login", {
-      username: username,
-      password: password
+      username,
+      password
     })
     .then((res) => {
       if (res.status === 200){
         localStorage.setItem("token", res.data);
       } else {
         setFailedLogin(true);
-        console.log("Login Failed");
       }
-      console.log(res.data);
-    }, (err) => {
-      console.log(err);
+    }, () => {
+      setFailedLogin(true);
     });
-
     setPassword("");
   };
 
   /*
    * Gets the password correct state of the password field, depending on if a previous login attempt failed
    */
-  function getPasswordField() {
+  const getPasswordField: VoidFunction = () => {
     if (failedLogin) {
       return (
-        <Textfield error className={classes.inputfield} id="password" label="Passwort" type="password" helperText='Passwort oder Benutzername sind nicht korrekt' value={password}
-          onChange = {event => {setPassword(event.target.value)}} onKeyUp={handleKeyUp} fullWidth />
+        <Textfield error className={classes.inputfield} id="password" label="Passwort" type="password"
+          helperText="Passwort oder Benutzername sind nicht korrekt" value={password}
+          onChange = {event => {setPassword(event.target.value);}}
+          onKeyUp={event => {handleKeyUp(event.getModifierState("CapsLock"));}} fullWidth />
       );
     }
     return (
       <Textfield className={classes.inputfield} id="password" label="Passwort" type="password" value={password}
-        onChange = {event => {setPassword(event.target.value)}} onKeyUp={handleKeyUp} fullWidth />
+        onChange = {event => {setPassword(event.target.value);}}
+        onKeyUp={event => {handleKeyUp(event.getModifierState("CapsLock"));}} fullWidth />
     );
-  }
+  };
 
   /**
    * Check if Capslock is enabled
    */
-  function handleKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
-    setCapslock(event.getModifierState('CapsLock'));
-  }
+  const handleKeyUp = (isCapsEnabled:boolean) => {
+    setCapslock(isCapsEnabled);
+  };
 
   /*
    * Warn if capslock is enabled
    */
-  function setCapsLockWaring() {
+  const setCapsLockWaring = () => {
     if (capslock) {
-      return 'Caps Lock is enabled!';
+      return "Caps Lock is enabled!";
     }
-    return '';
-  } 
+    return "";
+  };
 
   return (
     <div className="login">
@@ -131,9 +129,10 @@ const Login: React.FunctionComponent = () => {
           <Paper className={classes.paper}>
             <img className={classes.logo} src={logo}/>
             <h1>Login</h1>
-            <form className={classes.login} onSubmit={event => {login()}}>
+            <form className={classes.login} onSubmit={event => {login();}}>
               <Textfield className={classes.inputfield} id="username" label="Benutzername" type="text" value={username}
-                onChange = {event => {setUsername(event.target.value)}} onKeyUp={handleKeyUp} fullWidth />
+                onChange = {event => {setUsername(event.target.value);}}
+                onKeyUp={event => {handleKeyUp(event.getModifierState("CapsLock"));}} fullWidth />
               {getPasswordField()}
               <Button className={classes.submit} variant="contained" fullWidth color="primary" type="submit">
                       Login
@@ -145,7 +144,7 @@ const Login: React.FunctionComponent = () => {
                   </Link>
                 </Grid>
                 <Grid item xs className={classes.warningItem}>
-                  <Link id='capswarning' variant="body2" className={classes.warningText}>
+                  <Link id="capswarning" variant="body2" className={classes.warningText}>
                     {setCapsLockWaring()}
                   </Link>
                 </Grid>
