2ada1df90ba33c5f16d6b2b24835d5bb81b84613
qnewald <newaldquentin@googlemail.com>
2020-04-14 17:43:40 +0200

    Implemented login page

--- client/src/ts/members/Login.tsx
+++ client/src/ts/members/Login.tsx
@@ -1,20 +1,72 @@
 /**
  * Component that handles the login process
  */
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import api from "../utils/api";
-
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
+import api from '../utils/api';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Button from "@material-ui/core/Button";
 import Textfield from "@material-ui/core/TextField";
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../../images/jbt-logo-black.png';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
 const Login: React.FunctionComponent = () => {
 
+  /**
+   * Styles
+   */
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      paddingBottom: theme.spacing(3),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    logo: {
+      width: '30%',
+      height: 'auto',
+      marginTop: theme.spacing(3),
+    },
+    login: {
+      width: '65%',
+      marginTop: theme.spacing(1),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    inputfield: {
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 1),
+    },
+    linkItem: {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+    warningItem: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    warningText: {
+      color: theme.palette.error.main,
+      textDecoration: 'none',
+      pointerEvents: 'none',
+    },
+  }));
+
+  const classes = useStyles();
+  
+  
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [capslock, setCapslock] = useState<boolean>(false);
+  const [failedLogin, setFailedLogin] = useState<boolean>(false);
 
   /**
    * Handles the API call and cleans state thereafter
@@ -25,9 +77,10 @@ const Login: React.FunctionComponent = () => {
       password: password
     })
     .then((res) => {
-      if (res.status = 200){
+      if (res.status === 200){
         localStorage.setItem("token", res.data);
       } else {
+        setFailedLogin(true);
         console.log("Login Failed");
       }
       console.log(res.data);
@@ -35,29 +88,72 @@ const Login: React.FunctionComponent = () => {
       console.log(err);
     });
 
-    setUsername("");
     setPassword("");
   };
 
+  /*
+   * Gets the password correct state of the password field, depending on if a previous login attempt failed
+   */
+  function getPasswordField() {
+    if (failedLogin) {
+      return (
+        <Textfield error className={classes.inputfield} id="password" label="Passwort" type="password" helperText='Passwort oder Benutzername sind nicht korrekt' value={password}
+          onChange = {event => {setPassword(event.target.value)}} onKeyUp={handleKeyUp} fullWidth />
+      );
+    }
+    return (
+      <Textfield className={classes.inputfield} id="password" label="Passwort" type="password" value={password}
+        onChange = {event => {setPassword(event.target.value)}} onKeyUp={handleKeyUp} fullWidth />
+    );
+  }
+
+  /**
+   * Check if Capslock is enabled
+   */
+  function handleKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
+    setCapslock(event.getModifierState('CapsLock'));
+  }
+
+  /*
+   * Warn if capslock is enabled
+   */
+  function setCapsLockWaring() {
+    if (capslock) {
+      return 'Caps Lock is enabled!';
+    }
+    return '';
+  } 
+
   return (
     <div className="login">
-      <Card className="card">
-        <CardContent>
-          <h1>Login</h1>
-          <form className="login-form">
-            <Textfield id="username" label="Benutzername" type="text" value={username}
-              onChange = {event => {setUsername(event.target.value);}}/>
-            <Textfield id="password" label="Passwort" type="password" value={password}
-              onChange = {event => {setPassword(event.target.value);}}/>
-          </form>
-        </CardContent>
-        <CardActions>
-          <Button variant="contained" size="small" color="primary"
-            onClick = {event => {login();}}>
-            Login
-          </Button>
-        </CardActions>
-      </Card>
+      <Grid container spacing={0} alignItems="center" justify="center">
+        <Grid item xs={10} sm={8} md={6} lg={4}>
+          <Paper className={classes.paper}>
+            <img className={classes.logo} src={logo}/>
+            <h1>Login</h1>
+            <form className={classes.login} onSubmit={event => {login()}}>
+              <Textfield className={classes.inputfield} id="username" label="Benutzername" type="text" value={username}
+                onChange = {event => {setUsername(event.target.value)}} onKeyUp={handleKeyUp} fullWidth />
+              {getPasswordField()}
+              <Button className={classes.submit} variant="contained" fullWidth color="primary" type="submit">
+                      Login
+              </Button>
+              <Grid container>
+                <Grid item xs className={classes.linkItem}>
+                  <Link href="#/login" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item xs className={classes.warningItem}>
+                  <Link id='capswarning' variant="body2" className={classes.warningText}>
+                    {setCapsLockWaring()}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
