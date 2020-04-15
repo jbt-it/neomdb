1c76de9b2373743a40b17a3f9e9f3552afb2858b
christophtrybek <christoph.trybek@gmail.com>
2020-04-15 12:31:12 +0200

    Added NotFound page
    Added routing destination for unused urls

--- client/src/ts/global/App.tsx
+++ client/src/ts/global/App.tsx
@@ -5,6 +5,7 @@ import decode from "jwt-decode";
 import Dashboard from "../members/Dashboard";
 import Login from "../members/Login";
 import Nav from "./navigation/Nav";
+import NotFound from "./NotFound";
 
 const App: React.FunctionComponent = () => {
 
@@ -71,6 +72,7 @@ const App: React.FunctionComponent = () => {
         <PrivateRoute exact path = "/weitere-funktionen" component = {Dashboard} />
         <PrivateRoute exact path = "/kvp" component = {Dashboard} />
         <Route exact path = "/login" component = {Login} />
+        <Route path = "*" component = {NotFound} />
       </Switch>
     </HashRouter>
   );
