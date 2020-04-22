8170decbb6978dae8b6b946905bf7933fa140c2d
BorchBe99 <55837643+BorchBe99@users.noreply.github.com>
2020-04-22 20:04:20 +0200

    Added Headline and Button

--- client/src/ts/global/NotFound.tsx
+++ client/src/ts/global/NotFound.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 const NotFound: React.FunctionComponent = () => {
-
+  /*
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+  
+  */
   return (
     <div className="App">
-      <h1>Error: Page not found!</h1>
+      <span style={{
+        position: 'relative',
+        top: 140,
+        left: 600
+      }}>
+        <h1>Fehler: Diese Seite konnte nicht gefunden werden! 404</h1>
+        <Button variant="contained" color="primary" href="./">Zurück zum Dashboard</Button>
+      </span>
     </div>
   );
 };
