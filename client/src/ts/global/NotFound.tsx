f9ce1fca760c64598467584b1a4a0b9542553385
BorchBe99 <55837643+BorchBe99@users.noreply.github.com>
2020-09-27 22:10:17 +0200

    Added responsive breakpoints

--- client/src/ts/global/NotFound.tsx
+++ client/src/ts/global/NotFound.tsx
@@ -1,41 +1,70 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, Theme, createStyles} from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  root: {
-    backgroundColor: "white",
-    minWidth: 250,
-    maxWidth: 330,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  notFound: {
-    backgroundColor: "white",
-    position: "absolute",
-    left: "35%",
-    top: "20%",
-  },
-});
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    title: {
+      fontSize: 17,
+    },
+    pos: {
+      marginBottom: 10,
+    },
+    notFoundCard: {
+      backgroundColor: "white",
+      [theme.breakpoints.up("xl")]: {
+        transform: "scale(1.1, 1.1)",
+        maxWidth: "45%",
+        marginTop: "5%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+      [theme.breakpoints.only("lg")]: {
+        transform: "scale(1.1, 1.1)",
+        maxWidth: "50%",
+        marginTop: "5%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+      [theme.breakpoints.only("md")]: {
+        transform: "scale(1.1, 1.1)",
+        maxWidth: "470px",
+        marginTop: "10%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+      [theme.breakpoints.only("sm")]: {
+        maxWidth: "430px",
+        marginTop: "10%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+      [theme.breakpoints.down("xs")]: {
+        maxWidth: "430px",
+        marginTop: "10%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+    },
+    backButton: {
+      color: "white",
+      backgroundColor: "#f6891f",
+    },
+  }));
 const NotFound: React.FunctionComponent = () => {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+
   return (
-    <div className={classes.notFound}>
-      <Card className={classes.root}>
+    <div className={"content-page ${classes.root}"}>
+      <Card className={classes.notFoundCard} variant="outlined">
         <CardContent>
           <Typography className={classes.title} color="textSecondary" gutterBottom>
             Fehler:
@@ -53,7 +82,7 @@ const NotFound: React.FunctionComponent = () => {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small" variant="contained" color="primary" href="./">Zurück zum Dashboard</Button>
+          <Button className={classes.backButton} size="medium" variant="contained" disableElevation href="./">Zurück zum Dashboard</Button>
         </CardActions>
       </Card>
     </div>
