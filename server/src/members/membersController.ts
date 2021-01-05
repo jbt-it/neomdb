e43989912eda795c1fa3c2b6f16bb6f71ac3d292
christophtrybek <christoph.trybek@gmail.com>
2021-01-05 17:49:53 +0100

    Fixed member can not update vertiefungen

--- server/src/members/membersController.ts
+++ server/src/members/membersController.ts
@@ -172,7 +172,7 @@ export const updateMember = (req: Request, res: Response): void => {
       mitglied_seit = ?, alumnus_seit = ?, senior_seit = ?, aktiv_seit = ?, passiv_seit = ?,
       ausgetreten_seit = ?, ressort = ?, arbeitgeber = ?, strasse1 = ?, plz1 = ?, ort1 = ?, tel1 = ?,
       email1 = ?, strasse2 = ?, plz2 = ?, ort2 = ?, tel2 = ?, email2 = ?, hochschule = ?, studiengang = ?,
-      studienbeginn = ?, studienende = ?, ausbildung = ?, kontoinhaber = ?, iban = ?, bic = ?,
+      studienbeginn = ?, studienende = ?, vertiefungen = ?, ausbildung = ?, kontoinhaber = ?, iban = ?, bic = ?,
       engagement = ?, canPL = ?, canQM = ?, lastchange = ?, fuehrerschein = ?, ersthelferausbildung = ?
       WHERE mitgliedID = ?`,
     [req.body.handy, req.body.mitgliedstatus, req.body.generation, req.body.internesprojekt,
@@ -181,8 +181,9 @@ export const updateMember = (req: Request, res: Response): void => {
       req.body.ressort, req.body.arbeitgeber, req.body.strasse1, req.body.plz1, req.body.ort1,
       req.body.tel1, req.body.email1, req.body.strasse2, req.body.plz2, req.body.ort2, req.body.tel2,
       req.body.email2, req.body.hochschule, req.body.studiengang, req.body.studienbeginn,
-      req.body.studienende, req.body.ausbildung, req.body.kontoinhaber, req.body.iban, req.body.bic,
-      req.body.engagement, req.body.canPL, req.body.canQM, lastChangeTime, req.body.fuehrerschein,
+      req.body.studienende, req.body.vertiefungen, req.body.ausbildung, req.body.kontoinhaber,
+      req.body.iban, req.body.bic, req.body.engagement, req.body.canPL, req.body.canQM, lastChangeTime,
+      req.body.fuehrerschein,
       req.body.ersthelferausbildung, req.params.id])
     .then((result) => {
       res.status(200).send("Profile Update Successful");
@@ -198,13 +199,13 @@ export const updateMember = (req: Request, res: Response): void => {
       `UPDATE mitglied
       SET handy = ?, arbeitgeber = ?, strasse1 = ?, plz1 = ?, ort1 = ?, tel1 = ?, email1 = ?, strasse2 = ?,
       plz2 = ?, ort2 = ?, tel2 = ?, email2 = ?, hochschule = ?, studiengang = ?, studienbeginn = ?,
-      studienende = ?, ausbildung = ?, kontoinhaber = ?, iban = ?, bic = ?, lastchange = ?, fuehrerschein = ?,
-      ersthelferausbildung = ?
+      studienende = ?, vertiefungen = ?, ausbildung = ?, kontoinhaber = ?, iban = ?, bic = ?, lastchange = ?,
+      fuehrerschein = ?, ersthelferausbildung = ?
       WHERE mitgliedID = ?`,
     [req.body.handy, req.body.arbeitgeber, req.body.strasse1, req.body.plz1, req.body.ort1, req.body.tel1,
       req.body.email1, req.body.strasse2, req.body.plz2, req.body.ort2, req.body.tel2, req.body.email2,
-      req.body.hochschule, req.body.studiengang, req.body.studienbeginn, req.body.studienende, req.body.ausbildung,
-      req.body.kontoinhaber, req.body.iban, req.body.bic, lastChangeTime, req.body.fuehrerschein,
+      req.body.hochschule, req.body.studiengang, req.body.studienbeginn, req.body.studienende, req.body.vertiefungen,
+      req.body.ausbildung, req.body.kontoinhaber, req.body.iban, req.body.bic, lastChangeTime, req.body.fuehrerschein,
       req.body.ersthelferausbildung, req.params.id])
     .then((result) => {
       res.status(200).send("Profile Update Successful");
