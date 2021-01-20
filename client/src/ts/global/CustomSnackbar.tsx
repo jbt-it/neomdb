cc0498625e36339d72137549c3d340e5bc58efb4
BorchBe99 <55837643+BorchBe99@users.noreply.github.com>
2021-01-20 20:38:04 +0100

    Fixed indentation to 2 Spaces

--- client/src/ts/global/CustomSnackbar.tsx
+++ client/src/ts/global/CustomSnackbar.tsx
@@ -8,41 +8,41 @@ import CloseIcon from "@material-ui/icons/Close";
 import { SvgIconProps } from "@material-ui/core/SvgIcon";
 
 interface Props {
-    snackbarMessage: SnackbarMessage;
-    snackProps?: OptionsObject;
-    buttonProps?: IconButtonProps;
-    iconProps?: SvgIconProps;
-    showID?: boolean;
+  snackbarMessage: SnackbarMessage;
+  snackProps?: OptionsObject;
+  buttonProps?: IconButtonProps;
+  iconProps?: SvgIconProps;
+  showID?: boolean;
 }
 const CustomSnackbar = (props: Props) => {
-    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-    /**
-     * Default close button for Snackbar with Close Icon.
-     */
-    const action = (key: string | number) => (
-        <React.Fragment>
-            <IconButton onClick={() => { closeSnackbar(key); }} {...props.buttonProps}>
-                <CloseIcon {...props.iconProps} />
-            </IconButton>
-        </React.Fragment>
-    );
-    /**
-     * Calls a Snackbar. When showID is set to true,
-     * it will return the ID of the Snackbar as React.ReactText.
-     */
-    const wrapperEnqueueSnackbar = () => {
-        const output = enqueueSnackbar(props.snackbarMessage, {
-            action,
-            ...props.snackProps,
-        });
-        if (props.showID) {
-            return (output);
-        }
-        return (null);
-    };
-    return (
-        <div>
-            {wrapperEnqueueSnackbar()}
-        </div>);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  /**
+   * Default close button for Snackbar with Close Icon.
+   */
+  const action = (key: string | number) => (
+    <React.Fragment>
+      <IconButton onClick={() => { closeSnackbar(key); }} {...props.buttonProps}>
+        <CloseIcon {...props.iconProps} />
+      </IconButton>
+    </React.Fragment>
+  );
+  /**
+   * Calls a Snackbar. When showID is set to true,
+   * it will return the ID of the Snackbar as React.ReactText.
+   */
+  const wrapperEnqueueSnackbar = () => {
+    const output = enqueueSnackbar(props.snackbarMessage, {
+      action,
+      ...props.snackProps,
+    });
+    if (props.showID) {
+      return (output);
+    }
+    return (null);
+  };
+  return (
+    <div>
+      {wrapperEnqueueSnackbar()}
+    </div>);
 };
 export default CustomSnackbar;
