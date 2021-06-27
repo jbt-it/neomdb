6f47996195b836e491e13a6d96cb829501a1d6b7
LennartRukower <mail@lennart-rukower.de>
2021-06-27 11:30:06 +0200

    Added custom route for login

--- client/src/ts/global/App.tsx
+++ client/src/ts/global/App.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import {HashRouter, Route, Switch, Redirect} from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import decode from "jwt-decode";
-import {AuthContext} from "../global/AuthContext";
+import { AuthContext } from "../global/AuthContext";
 import Dashboard from "../members/Dashboard";
 import MemberOverview from "../members/MemberOverview";
 import Login from "../members/Login";
@@ -11,14 +11,14 @@ import ChangePassword from "../members/ChangePassword";
 
 const App: React.FunctionComponent = () => {
   const [authenticated, setAuthenticated,
-        userID, setUserID, userName, setUserName] = useContext(AuthContext);
+    userID, setUserID, userName, setUserName] = useContext(AuthContext);
 
   /**
    * Checks if token in local storage is set or expired
    */
   const checkAuth = (): boolean => {
     const token = localStorage.getItem("token");
-    if (!token){
+    if (!token) {
       setAuthenticated(false);
       setUserID(null);
       setUserName(null);
@@ -53,43 +53,59 @@ const App: React.FunctionComponent = () => {
    * Renders the specified component if the user is authenticated otherwise
    * the user gets redirected to the login page
    */
-  const PrivateRoute = ({component: Component, ...rest}: any) => {
-    return(
-      <Route {...rest} render = {props => (
+  const PrivateRoute = ({ component: Component, ...rest }: any) => {
+    return (
+      <Route {...rest} render={props => (
         checkAuth() ? (
-          <Component {...props}/>
+          <Component {...props} />
         ) : (
-          <Redirect to={{pathname: "/login"}}/>
+          <Redirect to={{ pathname: "/login" }} />
         )
-      )}/>
+      )} />
+    );
+  };
+
+  /**
+   * Renders the login component if the user is not authenticated otherwise
+   * the user gets redirected to the dashboard page
+   */
+  const LoginRoute = ({ component: Component, ...rest }: any) => {
+    return (
+      <Route {...rest} render={props => (
+        !checkAuth() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/" }} />
+        )
+      )} />
     );
   };
 
   return (
-      <HashRouter>
+    <HashRouter>
       {
-      // Renders the Nav componenent if the user is authenticated
-      (authenticated ? <Nav/> : null)
+        // Renders the Nav componenent if the user is authenticated
+        (authenticated ? <Nav /> : null)
       }
       <Switch>
-        <PrivateRoute exact path = "/user-change-password" component = {ChangePassword} />
-        <PrivateRoute exact path = "/" component = {Dashboard} />
-        <PrivateRoute exact path = "/gesamtuebersicht" component = {MemberOverview} />
-        <PrivateRoute exact path = "/vorstand" component = {Dashboard} />
-        <PrivateRoute exact path = "/geburtstage" component = {Dashboard} />
-        <PrivateRoute exact path = "/traineebereich" component = {Dashboard} />
-        <PrivateRoute exact path = "/kuratoren" component = {Dashboard} />
-        <PrivateRoute exact path = "/projekte" component = {Dashboard} />
-        <PrivateRoute exact path = "/veranstaltungen" component = {Dashboard} />
-        <PrivateRoute exact path = "/mm-tracking" component = {Dashboard} />
-        <PrivateRoute exact path = "/pl-qm-tool" component = {Dashboard} />
-        <PrivateRoute exact path = "/raumreservierung" component = {Dashboard} />
-        <PrivateRoute exact path = "/innovationsmanagement" component = {Dashboard} />
-        <PrivateRoute exact path = "/meine-funktionen" component = {Dashboard} />
-        <PrivateRoute exact path = "/weitere-funktionen" component = {Dashboard} />
-        <PrivateRoute exact path = "/kvp" component = {Dashboard} />
-        <Route exact path = "/login" component = {Login} />
-        <PrivateRoute path = "*" component = {NotFound} />
+        <PrivateRoute exact path="/user-change-password" component={ChangePassword} />
+        <PrivateRoute exact path="/" component={Dashboard} />
+        <PrivateRoute exact path="/gesamtuebersicht" component={MemberOverview} />
+        <PrivateRoute exact path="/vorstand" component={Dashboard} />
+        <PrivateRoute exact path="/geburtstage" component={Dashboard} />
+        <PrivateRoute exact path="/traineebereich" component={Dashboard} />
+        <PrivateRoute exact path="/kuratoren" component={Dashboard} />
+        <PrivateRoute exact path="/projekte" component={Dashboard} />
+        <PrivateRoute exact path="/veranstaltungen" component={Dashboard} />
+        <PrivateRoute exact path="/mm-tracking" component={Dashboard} />
+        <PrivateRoute exact path="/pl-qm-tool" component={Dashboard} />
+        <PrivateRoute exact path="/raumreservierung" component={Dashboard} />
+        <PrivateRoute exact path="/innovationsmanagement" component={Dashboard} />
+        <PrivateRoute exact path="/meine-funktionen" component={Dashboard} />
+        <PrivateRoute exact path="/weitere-funktionen" component={Dashboard} />
+        <PrivateRoute exact path="/kvp" component={Dashboard} />
+        <LoginRoute exact path="/login" component={Login} />
+        <PrivateRoute path="*" component={NotFound} />
       </Switch>
     </HashRouter>
   );
