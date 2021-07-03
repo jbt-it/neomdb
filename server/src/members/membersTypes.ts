d15e3ac0179bce7ab5008039d1ff61fdff163086
LennartRukower <mail@lennart-rukower.de>
2021-07-03 23:55:36 +0200

    Deleted LoginQueryResult from membersTypes

--- server/src/members/membersTypes.ts
+++ server/src/members/membersTypes.ts
@@ -1,13 +1,6 @@
 /**
  * Definition of used types within the users module
  */
-export type LoginQueryResult = {
-  mitgliedID: number;
-  name: string;
-  passwordHash: string;
-  permissions: string;
-};
-
 export type GetMembersQueryResult = {
   vorname: string;
   nachname: string;
