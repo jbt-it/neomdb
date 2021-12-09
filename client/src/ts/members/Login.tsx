fad4b18549b9ba22d6a9fc3a0770ad2478333fb0
Johannes Braun <johannes.f.braun@gmail.com>
2021-12-09 10:53:17 +0100

    fixed nav link to page

--- client/src/ts/members/Login.tsx
+++ client/src/ts/members/Login.tsx
@@ -151,7 +151,7 @@ const Login: React.FunctionComponent = () => {
               </Button>
               <Grid container>
                 <Grid item xs className={classes.linkItem}>
-                  <NavLink exact to="/forgot-password" >
+                  <NavLink exact to="/passwort-vergessen" >
                     Forgot Password?
                   </NavLink>
                 </Grid>
