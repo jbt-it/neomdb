a4915a0d13737eb533dd0717cae6559bddd83b36
JuicySchiss <jan_mueller31@yahoo.de>
2022-11-18 15:58:14 +0100

    Removed retrieveGenerations and fixed formatting

--- server/src/trainees/traineesController.ts
+++ server/src/trainees/traineesController.ts
@@ -1,44 +1,49 @@
 /**
  * Definition of the handler functions for the members module
  */
- import database = require("../database");
- import bcrypt = require("bcryptjs");
- import { PoolConnection } from "mysql";
- 
- import { Request, Response } from "express";
- import * as traineesTypes from "./traineesTypes";
- 
+import database = require("../database");
+import bcrypt = require("bcryptjs");
+import { PoolConnection } from "mysql";
+
+import { Request, Response } from "express";
+import * as traineesTypes from "./traineesTypes";
 
 /**
  * Retrieves all trainees
  */
 export const retrieveCurrentTrainees = (req: Request, res: Response): void => {
-    database.query(
+  database
+    .query(
       `SELECT mitgliedID, vorname, nachname
         FROM mitglied
         WHERE mitgliedstatus = 1 AND mitglied.generation = (SELECT max(generation) from internesprojekt)
-        ORDER BY nachname`, [])
-      .then((result: traineesTypes.GetCurrentTraineesQueryResult[]) => {
-        if (result.length === 0) {
-          res.status(404).send("Trainees not found");
-        } else {
-          res.status(200).json(result);
-        }
-      })
-      .catch((err) => {
-        res.status(500).send("Query Error");
-      });
-  };
+        ORDER BY nachname`,
+      []
+    )
+    .then((result: traineesTypes.GetCurrentTraineesQueryResult[]) => {
+      if (result.length === 0) {
+        res.status(404).send("Trainees not found");
+      } else {
+        res.status(200).json(result);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Query Error");
+    });
+};
 
-  /**
- * Retrieves all current IPs
- */
+/*
+  Retrieves all current IPs
+  */
 export const retrieveCurrentIPs = (req: Request, res: Response): void => {
-  database.query(
-    `SELECT internesprojektID, projektname, kickoff, AngebotBeiEV, ZPbeiEV, APgehalten
+  database
+    .query(
+      `SELECT internesprojektID, projektname, kickoff, AngebotBeiEV, ZPbeiEV, APgehalten
       FROM internesprojekt
       WHERE generation = (SELECT max(generation) from internesprojekt)
-      ORDER BY internesprojektID`, [])
+      ORDER BY internesprojektID`,
+      []
+    )
     .then((result: traineesTypes.GetCurrentTraineesQueryResult[]) => {
       if (result.length === 0) {
         res.status(404).send("IPs not found");
@@ -50,40 +55,3 @@ export const retrieveCurrentIPs = (req: Request, res: Response): void => {
       res.status(500).send("Query Error");
     });
 };
-
-
-
-  /**
- * Retrieves all trainee generations
- * SELECT MAX(generationID) FROM generation; 
- * For all i to Max(generationID) create type?
- */
-export const retrieveTraineeGenerations = (req: Request, res: Response): void => {
-    //numberOfGenerations = database.query(SELECT MAX(generationID) FROM generation)
-    
-    let numberOfGenerations: number;
-    database.query('SELECT MAX(generationID) FROM generation',[]).then((result: number)=>{
-        numberOfGenerations = result;
-    });
- 
-    for (var i = 1; i< numberOfGenerations + 1;i++){
-        database.query(
-            `SELECT mitgliedID, vorname, nachname
-              FROM mitglied
-              WHERE generationID =` +i+
-
-              //hier noch Projekte
-
-              `ORDER BY nachname`, [])
-            .then((result: traineesTypes.GetCurrentTraineesQueryResult[]) => {
-              if (result.length === 0) {
-                res.status(404).send("Trainees not found");
-              } else {
-                res.status(200).json(result);
-              }
-            })
-            .catch((err) => {
-              res.status(500).send("Query Error");
-            });
-    };
-  };
--- server/src/trainees/traineesRoutes.ts
+++ server/src/trainees/traineesRoutes.ts
@@ -1,21 +1,19 @@
 /**
  * Definition of the API routes and invocation of the correct handler function
  */
- import express = require("express");
- const router = express.Router();
- 
- import * as authController from "../global/auth/authController";
- import * as traineesController from "./traineesController";
- 
+import express = require("express");
+const router = express.Router();
+
+import * as authController from "../global/auth/authController";
+import * as traineesController from "./traineesController";
 
 /**
  * =======>>> ALL routes after this point are accessible for logged in users only <<<=======
  */
- router.use(authController.protectRoutes);
- 
- //  =======>>> Get routes <<<=======
- router.get("/", traineesController.retrieveCurrentTrainees);
- router.get("/currentIPs", traineesController.retrieveCurrentIPs);
+router.use(authController.protectRoutes);
 
- 
- module.exports = router;
+//  =======>>> Get routes <<<=======
+router.get("/", traineesController.retrieveCurrentTrainees);
+router.get("/current-ips", traineesController.retrieveCurrentIPs);
+
+module.exports = router;
