d88503527da0cedcd2b8783038cc42d2d6f767ab
Benjamin Borchert <benjamin.benji20@gmail.com>
2023-05-20 17:00:51 +0200

    Deleted notistack and SnackbarProvider Lines

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,22 +5,13 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import globalTheme from "./ts/utils/globalTheme";
 import { AuthProvider } from "./ts/global/AuthContext";
 import "./css/app.css";
-import { SnackbarProvider } from "notistack";
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.render(
   <AuthProvider>
     <ThemeProvider theme={globalTheme}>
-      <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        hideIconVariant={false}
-        dense={false}
-        preventDuplicate={false}
-      >
-        <App />
-        <Toaster position="bottom-center" reverseOrder={true} />
-      </SnackbarProvider>
+      <App />
+      <Toaster position="bottom-center" reverseOrder={true} />
     </ThemeProvider>
   </AuthProvider>,
   document.getElementById("root")
