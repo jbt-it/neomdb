c96c721254c50caa299b9f8af337145cc838706d
LennartRukower <mail@lennart-rukower.de>
2023-05-23 16:14:44 +0200

    feat(middleware): Fixed authentication

--- server/src/middleware/authentication.ts
+++ server/src/middleware/authentication.ts
@@ -5,7 +5,7 @@ import { verifyJWT } from "../utils/jwtUtils";
  * Implements the authentication middleware which **protects** all critical routes from unauthorised access.
  */
 const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.cookies || verifyJWT(req.cookies.token)) {
+  if (!req.cookies || !req.cookies.token || !verifyJWT(req.cookies.token)) {
     return res.status(401).send("Authentication failed: Please log in");
   }
   const jwtData = verifyJWT(req.cookies.token);
