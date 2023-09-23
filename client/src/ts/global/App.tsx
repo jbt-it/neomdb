5f8790be3d9bbdc2319e12afc182082dade9f767
qnewald <newaldquentin@googlemail.com>
2023-09-23 14:30:26 +0200

    Added localization boilerplate for Date Pickers

--- client/src/ts/global/App.tsx
+++ client/src/ts/global/App.tsx
@@ -19,6 +19,8 @@ import LoadingCircle from "./components/LoadingCircle";
 import { doesPermissionsHaveSomeOf } from "../utils/authUtils";
 import TraineePreferences from "../trainees/TraineePreferences";
 import FieldSectionTest from "./FieldSectionTest";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 /**
  * Interfaces for the location state of react-router-dom
@@ -152,38 +154,40 @@ const App: React.FunctionComponent = () => {
   });
 
   return (
-    <HashRouter>
-      {
-        // Renders the Nav componenent if the user is authenticated
-        auth.authenticated ? <Nav /> : null
-      }
-      <Switch>
-        <PrivateRoute exact path="/user-change-password" component={ChangePassword} />
-        <PrivateRoute exact path="/" component={Dashboard} />
-        <PrivateRoute exact path="/gesamtuebersicht" component={MemberOverview} />
-        <PrivateRoute exact path="/ressorts" component={DepartmentOverview} />
-        <PrivateRoute exact path="/ewigervorstand" component={DirectorsHistory} />
-        <PrivateRoute exact path="/geburtstage" component={Dashboard} />
-        <PrivateRoute exact path="/traineebereich" component={Dashboard} />
-        <PrivateRoute exact path="/kuratoren" component={Dashboard} />
-        <PrivateRoute exact path="/projekte" component={Dashboard} />
-        <PrivateRoute exact path="/veranstaltungen" component={Dashboard} />
-        <PrivateRoute exact path="/mm-tracking" component={Dashboard} />
-        <PrivateRoute exact path="/pl-qm-tool" component={Dashboard} />
-        <PrivateRoute exact path="/raumreservierung" component={Dashboard} />
-        <PrivateRoute exact path="/innovationsmanagement" component={Dashboard} />
-        <PrivateRoute exact path="/meine-funktionen" component={Dashboard} />
-        <PrivateRoute exact path="/weitere-funktionen" component={Dashboard} />
-        <PrivateRoute exact path="/mitgliederverwaltung" component={MemberManagement} />
-        <PrivateRoute exact path="/traineepreferenzen" component={TraineePreferences} />
-        <PrivateRoute exact path="/test" component={FieldSectionTest} />
-        <PrivateRoute exact path="/kvp" component={Dashboard} />
-        <ProtectedRoute exact path="/berechtigungen" component={PermissionsOverview} permissionIDs={[]} />
-        <PrivateRoute exact path="/gesamtuebersicht/:id" component={MemberProfile} />
-        <LoginRoute exact path="/login" component={Login} />
-        <PrivateRoute path="*" component={NotFound} />
-      </Switch>
-    </HashRouter>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <HashRouter>
+        {
+          // Renders the Nav componenent if the user is authenticated
+          auth.authenticated ? <Nav /> : null
+        }
+        <Switch>
+          <PrivateRoute exact path="/user-change-password" component={ChangePassword} />
+          <PrivateRoute exact path="/" component={Dashboard} />
+          <PrivateRoute exact path="/gesamtuebersicht" component={MemberOverview} />
+          <PrivateRoute exact path="/ressorts" component={DepartmentOverview} />
+          <PrivateRoute exact path="/ewigervorstand" component={DirectorsHistory} />
+          <PrivateRoute exact path="/geburtstage" component={Dashboard} />
+          <PrivateRoute exact path="/traineebereich" component={Dashboard} />
+          <PrivateRoute exact path="/kuratoren" component={Dashboard} />
+          <PrivateRoute exact path="/projekte" component={Dashboard} />
+          <PrivateRoute exact path="/veranstaltungen" component={Dashboard} />
+          <PrivateRoute exact path="/mm-tracking" component={Dashboard} />
+          <PrivateRoute exact path="/pl-qm-tool" component={Dashboard} />
+          <PrivateRoute exact path="/raumreservierung" component={Dashboard} />
+          <PrivateRoute exact path="/innovationsmanagement" component={Dashboard} />
+          <PrivateRoute exact path="/meine-funktionen" component={Dashboard} />
+          <PrivateRoute exact path="/weitere-funktionen" component={Dashboard} />
+          <PrivateRoute exact path="/mitgliederverwaltung" component={MemberManagement} />
+          <PrivateRoute exact path="/traineepreferenzen" component={TraineePreferences} />
+          <PrivateRoute exact path="/test" component={FieldSectionTest} />
+          <PrivateRoute exact path="/kvp" component={Dashboard} />
+          <ProtectedRoute exact path="/berechtigungen" component={PermissionsOverview} permissionIDs={[]} />
+          <PrivateRoute exact path="/gesamtuebersicht/:id" component={MemberProfile} />
+          <LoginRoute exact path="/login" component={Login} />
+          <PrivateRoute path="*" component={NotFound} />
+        </Switch>
+      </HashRouter>
+    </LocalizationProvider>
   );
 };
 
