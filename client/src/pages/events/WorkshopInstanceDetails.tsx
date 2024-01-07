0d11fb27d9f06258df84c64a1b79ffaa879d6b65
jan_mueller31@yahoo.de <jan_mueller31@yahoo.de>
2024-01-07 19:09:02 +0100

    changed route to feedback overview

--- client/src/pages/events/WorkshopInstanceDetails.tsx
+++ client/src/pages/events/WorkshopInstanceDetails.tsx
@@ -193,7 +193,7 @@ const WorkshopInstanceDetails = () => {
     ) : workshopInstance.status === "Feedback" || workshopInstance.status === "Abgeschlossen" ? (
       <Button
         variant="contained"
-        href={`/#/workshops/${workshopInstance.schulungId}/${workshopInstance.schulungsinstanzID}/feedback`}
+        href={`/#/workshops/${workshopInstance.schulungId}/${workshopInstance.schulungsinstanzID}/feedbackauswertung`}
         sx={{ fontWeight: 600, fontSize: isMobile ? 10 : 14 }}
         color="info"
         startIcon={<Grade />}
