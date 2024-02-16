cb1b58ecaa36fa2782720b25d4b22f410db73e1d
LennartRukower <mail@lennart-rukower.de>
2024-02-16 17:46:37 +0100

    Feat(events): Added todos comment to update event service

--- server/src/resources/events/EventsService.ts
+++ server/src/resources/events/EventsService.ts
@@ -1,11 +1,26 @@
+import { UpdatedEvent } from "types/EventTypes";
 import EventsRepository from "./EventsRepository";
+import { executeInTransaction } from "database";
 
 class EventService {
   eventRepository = new EventsRepository();
 
-  updateEvent = async (eventID: number, updatedEvent: any): Promise<void> => {
+  updateEvent = async (eventID: number, updatedEvent: UpdatedEvent, eventMemberIDs: number[]): Promise<void> => {
+    // TODO: Get the current event data
+
+    // TODO: Get the current members of the event
+
     // TODO: Add validation
 
-    return this.eventRepository.updateEventByID(eventID, updatedEvent);
+    const tasks = [];
+    tasks.push({
+      func: this.eventRepository.updateEventByID,
+      args: [eventID, updatedEvent],
+    });
+    // TODO: Filter which members to add and which to delete from the event
+
+    await executeInTransaction(tasks);
   };
 }
+
+export default EventService;
