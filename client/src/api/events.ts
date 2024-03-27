1e9820282a212a97bf083aacadecfc197b751a63
JanMueller31 <jan_mueller31@yahoo.de>
2024-03-27 13:25:57 +0100

    Deleted events.ts since there is no backend route for the events yet

--- client/src/api/events.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosResponse } from "axios";
-import api from "../utils/api";
-import { CommonEventType } from "../types/eventTypes";
-
-// GET ROUTES
-export const getMembers = async (): Promise<AxiosResponse<CommonEventType[]>> => {
-  return await api.get<CommonEventType[]>("/members/");
-};
