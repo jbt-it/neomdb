25121083b7b922829612464660edcf2659e4187a
JanMueller31 <jan_mueller31@yahoo.de>
2024-04-09 20:08:44 +0200

    Added TableContainer

--- client/src/components/projects/AllProjectsTable.tsx
+++ client/src/components/projects/AllProjectsTable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ProjectOverview } from "../../types/projectTypes";
-import { Paper, useTheme, Table, TableCell, TableHead, TableRow } from "@mui/material";
+import { Paper, useTheme, Table, TableCell, TableHead, TableRow, TableContainer } from "@mui/material";
 import useResponsive from "../../hooks/useResponsive";
 import ProjectChip from "./projectCard/ProjectChip";
 import { Link } from "react-router-dom";
@@ -41,55 +41,64 @@ const AllProjectsTable = ({ projects }: AllProjectsTableProps) => {
   // Display a smaller table for mobile devices
   if (isMobile) {
     return (
-      <Table component={Paper} size="small">
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead sx={styles.tableHeader}>
+            <TableRow>
+              <TableCell sx={styles.tableHeaderCell}>Projektname</TableCell>
+              <TableCell sx={styles.tableHeaderCell}>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          {projects.map((project) => (
+            <TableRow
+              key={project.projectID}
+              sx={styles.tableRow}
+              component={Link}
+              to={`/projekt/${project.projectID}`}
+            >
+              <TableCell>{project.projectName}</TableCell>
+              <TableCell>
+                <ProjectChip status={project.projectStatus} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </Table>
+      </TableContainer>
+    );
+  }
+
+  // Display a larger table for desktop devices
+  return (
+    <TableContainer component={Paper}>
+      <Table size="small">
         <TableHead sx={styles.tableHeader}>
           <TableRow>
-            <TableCell sx={styles.tableHeaderCell}>ProjektName</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>Projektname</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>Branche</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>Unternehmen</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>Kernkompetenz</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>Beginn</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>Ende</TableCell>
+            <TableCell sx={styles.tableHeaderCell}>BT</TableCell>
             <TableCell sx={styles.tableHeaderCell}>Status</TableCell>
           </TableRow>
         </TableHead>
         {projects.map((project) => (
           <TableRow key={project.projectID} sx={styles.tableRow} component={Link} to={`/projekt/${project.projectID}`}>
             <TableCell>{project.projectName}</TableCell>
+            <TableCell>{project.projectSector}</TableCell>
+            <TableCell>{project.projectCompany}</TableCell>
+            <TableCell>{project.projectCoreCompetence}</TableCell>
+            <TableCell>{project.projectStartDate.format("DD.MM.YYYY")}</TableCell>
+            <TableCell>{project.projectEndDate.format("DD.MM.YYYY")}</TableCell>
+            <TableCell>{project.projectNumberOfBT}</TableCell>
             <TableCell>
               <ProjectChip status={project.projectStatus} />
             </TableCell>
           </TableRow>
         ))}
       </Table>
-    );
-  }
-
-  // Display a larger table for desktop devices
-  return (
-    <Table component={Paper} size="small">
-      <TableHead sx={styles.tableHeader}>
-        <TableRow>
-          <TableCell sx={styles.tableHeaderCell}>ProjektName</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>Branche</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>Unternehmen</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>Kernkompetenz</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>Beginn</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>Ende</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>BT</TableCell>
-          <TableCell sx={styles.tableHeaderCell}>Status</TableCell>
-        </TableRow>
-      </TableHead>
-      {projects.map((project) => (
-        <TableRow key={project.projectID} sx={styles.tableRow} component={Link} to={`/projekt/${project.projectID}`}>
-          <TableCell>{project.projectName}</TableCell>
-          <TableCell>{project.projectSector}</TableCell>
-          <TableCell>{project.projectCompany}</TableCell>
-          <TableCell>{project.projectCoreCompetence}</TableCell>
-          <TableCell>{project.projectStartDate.format("DD.MM.YYYY")}</TableCell>
-          <TableCell>{project.projectEndDate.format("DD.MM.YYYY")}</TableCell>
-          <TableCell>{project.projectNumberOfBT}</TableCell>
-          <TableCell>
-            <ProjectChip status={project.projectStatus} />
-          </TableCell>
-        </TableRow>
-      ))}
-    </Table>
+    </TableContainer>
   );
 };
 
